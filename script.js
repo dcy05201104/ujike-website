ab35ffa1527bec3127ef3b5c248f885af8f076e6
dcy05201104 <dcy.05201104@gmail.com>
2026-03-10 19:13:22 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+}, { threshold: 0.1 });
+
+const hiddenElements = document.querySelectorAll('.fade-in');
+hiddenElements.forEach((el) => observer.observe(el));
